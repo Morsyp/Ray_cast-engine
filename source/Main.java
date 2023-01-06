0d69041094f4785de285b4a4c857c5a607526ad0
Morsyp <111891258+Morsyp@users.noreply.github.com>
2023-01-06 20:26:03 +0200

    Update Main.java

--- source/Main.java
+++ source/Main.java
@@ -13,13 +13,13 @@ public class Main extends JComponent implements ActionListener {
 	Timer t = new Timer((int) (1000 / FPS), this);
 	static Random r = new Random();
 
-	public static final int SC_W = 800, SC_H = 600;
-	public static final int wallS = 50;
+	public static final int SC_W = 1000, SC_H = 800;
+	public static final int wallS = 100;
 
 	public static int px = 125, py = 125;
 	public static final double FOV = Math.PI * 2 / 6;
-	public static final double speeda = 0.1;
-	public static final int speedp = 4;
+	public static final double speeda = 0.05;
+	public static final int speedp = 10;
 	public static double pa = 0;
 
 	public static int[] dists = new int[SC_W/2];
@@ -42,6 +42,8 @@ public class Main extends JComponent implements ActionListener {
 			, {1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1}
 			, {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 			};
+	
+	private static boolean W_PRESSED, A_PRESSED, S_PRESSED, D_PRESSED;
 
 	public static void main(String[] args) {
 		JFrame f = new JFrame("ENGINE");
@@ -85,6 +87,27 @@ public class Main extends JComponent implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		getDists();
 		repaint();
+		move();
+	}
+
+	private void move() {
+		if(W_PRESSED) {
+			px += (int) (speedp * Math.cos(pa));
+			py += (int) (speedp * Math.sin(pa));
+		}
+		
+		if(A_PRESSED) {
+			pa -= speeda;
+		}
+		
+		if(S_PRESSED) {
+			px -= (int) (speedp * Math.cos(pa));
+			py -= (int) (speedp * Math.sin(pa));
+		}
+		
+		if(D_PRESSED) {
+			pa += speeda;
+		}
 	}
 
 	public void getDists() {
@@ -96,17 +119,23 @@ public class Main extends JComponent implements ActionListener {
 	public int getDist(int numOfDist) {
 
 		int step = 10;
-		int curx = px, cury = py; //FOV / dists.length
+		int curx = px, cury = py; // FOV / dists.length
 
 		while (true) {
-			curx += (int) (step * Math.cos(pa - FOV / 2 + (FOV / dists.length)*numOfDist));
-			cury += (int) (step * Math.sin(pa - FOV / 2 + (FOV / dists.length)*numOfDist));
+			curx += (int) (step * Math.cos(pa - FOV / 2 + (FOV / dists.length) * numOfDist));
+			cury += (int) (step * Math.sin(pa - FOV / 2 + (FOV / dists.length) * numOfDist));
 
 			for (int i = 0; i < map.length; i++) {
-				if (map[cury / wallS][curx / wallS] == 1) {
-					return (int) Math.sqrt(rect(curx - px) + rect(cury - py));
-				} else if(i > 1000) {
-					return (int) Math.sqrt(rect(curx - px) + rect(cury - py));
+				try {
+					if (map[cury / wallS][curx / wallS] == 1) {
+
+						return (int) Math.sqrt(rect(curx - px) + rect(cury - py));
+
+					} else if ((int) Math.sqrt(rect(curx - px) + rect(cury - py)) >= SC_H*2) {
+						return (int) Math.sqrt(rect(curx - px) + rect(cury - py));
+					}
+				} catch (Exception e) {
+					return 0;
 				}
 			}
 		}
@@ -114,18 +143,18 @@ public class Main extends JComponent implements ActionListener {
 	
 	
 	public void render(Graphics g) {
-		g.setColor(Color.DARK_GRAY);
+		g.setColor(new Color(170, 70, 0));
 		g.fillRect(0, SC_H/2, SC_W, SC_H/2);
 		
-		g.setColor(new Color(50, 50, 200));
+		g.setColor(new Color(0, 150, 255));
 		g.fillRect(0, 0, SC_W, SC_H/2);
 		
 		for(int i=0;i < dists.length;i++) {
 			if(dists[i] > SC_H) dists[i] = SC_H;
-			if(dists[i]/2 < 255) g.setColor(new Color(0, 0, 255-dists[i]/2));
+			if(dists[i]/4 < 255) g.setColor(new Color(255-dists[i]/4, 255-dists[i]/4, 0));
 			else g.setColor(new Color(0, 0, 0));
 			
-			g.fillRect(SC_W / dists.length * i, SC_H/2 - (SC_H-dists[i])/2, SC_W / dists.length, SC_H-dists[i]);
+			g.fillRect(SC_W / dists.length * i, SC_H/2 - (SC_H-dists[i]/2)/2, SC_W / dists.length, SC_H-dists[i]/2);
 		}
 	}
 
@@ -137,33 +166,43 @@ public class Main extends JComponent implements ActionListener {
 		@Override
 		public void keyPressed(KeyEvent e) {
 			switch (e.getKeyCode()) {
-
-			case KeyEvent.VK_RIGHT:
-				pa += speeda;
-				break;
-
-			case KeyEvent.VK_LEFT:
-				pa -= speeda;
-				break;
-
+			
 			case KeyEvent.VK_W:
-				px += (int) (speedp * Math.cos(pa));
-				py += (int) (speedp * Math.sin(pa));
+				W_PRESSED = true;
 				break;
 
 			case KeyEvent.VK_A:
-				pa -= speeda;
+				A_PRESSED = true;
 				break;
 
 			case KeyEvent.VK_S:
-				px += (int) (-speedp * Math.cos(pa));
-				py += (int) (-speedp * Math.sin(pa));
+				S_PRESSED = true;
 				break;
 
 			case KeyEvent.VK_D:
-				pa += speeda;
+				D_PRESSED = true;
+				break;
+			}
+		}
+		@Override
+		public void keyReleased(KeyEvent e) {
+			switch (e.getKeyCode()) {
+			
+			case KeyEvent.VK_W:
+				W_PRESSED = false;
 				break;
 
+			case KeyEvent.VK_A:
+				A_PRESSED = false;
+				break;
+
+			case KeyEvent.VK_S:
+				S_PRESSED = false;
+				break;
+
+			case KeyEvent.VK_D:
+				D_PRESSED = false;
+				break;
 			}
 		}
 	}
